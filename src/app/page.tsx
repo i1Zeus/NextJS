fe84b1d32994622f9c1447185db9db02825c26e0
Hussein Najah <husseinnajah123@gmail.com>
2023-07-12 18:22:05 +0300

    routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,15 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <header className="text-2xl">Todos</header>
+      <header className="text-2xl flex justify-between items-center mb-4">
+        <h1 className="text-2xl">Todos</h1>
+        <Link
+          href={"/new"}
+          className="bg-transparent border border-light-300 rounded-md px-2 py-1 hover:bg-slate-400 hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+        >
+          New
+        </Link>
+      </header>
     </main>
   );
 }
