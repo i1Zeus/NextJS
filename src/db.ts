0f29b0f482b59d323ddbfe1039ed5bee1ca8f26b
Hussein Najah <husseinnajah123@gmail.com>
2023-07-12 18:09:58 +0300

    routing

--- src/db.ts
+++ src/db.ts
@@ -1,1 +1,13 @@
 import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = global as unknown as {
+  pisma?: PrismaClient | undefined;
+};
+
+export const prisma =
+  globalForPrisma.pisma ??
+  new PrismaClient({
+    log: ["query"],
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.pisma = prisma;
